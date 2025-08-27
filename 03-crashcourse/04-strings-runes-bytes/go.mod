dd5d803dd69e1aec153cfba1598873f981be65dd
Rigan Mahmud Chowdhury <k2lu@Rigans-MacBook-Air.local>
2025-08-27 11:19:23 +0600

    feat: strings

---
+++ 03-crashcourse/04-strings-runes-bytes/go.mod
@@ -0,0 +1,3 @@
+module strings-runes-butes
+
+go 1.25.0
---
+++ 03-crashcourse/04-strings-runes-bytes/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var myString = "resume"
+	var indexed = myString[0]
+	fmt.Printf("%v, %T\n", indexed, indexed)
+
+	for i, v := range myString {
+		fmt.Println(i, v)
+	}
+}
